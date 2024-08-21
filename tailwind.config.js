01e21bb8e1391904a8a814e7686e934267098fdc
yafiirfan <yafiirfanzuhdi@gmail.com>
2024-08-21 19:09:07 +0700

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ['./index.html'],
+  theme: {
+    extend: {
+      colors : {
+          primary :'#14b8a6',
+          dark : '#020617',
+      }
+    },
+  },
+  plugins: [],
+};
+
