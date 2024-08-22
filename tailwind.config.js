f42f239964af8ecdec290dcf93cfac1320f31ad1
yafiirfan <yafiirfanzuhdi@gmail.com>
2024-08-22 19:19:43 +0700

    baru

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
+    container: {
+      center: true,
+      padding: '16px',
+    },
     extend: {
       colors : {
           primary :'#14b8a6',
+          secondary : '#64748b',
           dark : '#020617',
-      }
+      },
+      screens: {
+        '2xl':'1320px',
+      },
     },
   },
   plugins: [],
